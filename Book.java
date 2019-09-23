9385bcf48f3095a14ab4aac0f38fcee7715b3427
cmora1011 <g00252058@stu.raritanval.edu>
2019-09-23 01:01:54 -0400

    lab

--- Book.java
+++ Book.java
@@ -3,24 +3,98 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author Chris Mora
+ * @version 9/16/19
  */
-class Book
-{
-     // The fields.
+class Book {
+    
     private String author;
     private String title;
+    private int pages;
+    private int borrowed;
+    private String refNumber;
+    private boolean courseText;
 
-    /**
-     * Set the author and title fields when this object
-     * is constructed.
-     */
-    public Book(String bookAuthor, String bookTitle)
-    {
+    
+    public Book(String bookAuthor, String bookTitle, int bookPages, String refNumber, boolean course) {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
+        refNumber = "";
+        courseText = course;
     }
 
-    // Add the methods here ...
+    
+    public String getAuthor() {
+        return author;
+    }
+
+    
+    public String getTitle() {
+        return title;
+    }
+
+    
+    public void printAuthor() {
+        System.out.println("The Author is " + author);
+    }
+
+    
+    public void printTitle() {
+        System.out.println("The Title is " + title);
+    }
+
+    
+    public int getPages() {
+        return pages;
+    }
+
+    
+    public void SetRefNumber(String ref) {
+        if (ref.length() >= 3) {
+            refNumber = ref;
+        } else {
+            System.out.println("Sorry, Reference number needs to be at least 3 characters.");
+        }
+    }
+
+    
+    public String getRefNumber() {
+        return refNumber;
+    }
+
+    
+    public void borrow() {
+        borrowed = borrowed + 1;
+    }
+
+    
+    public int getBorrowed() {
+
+        return borrowed;
+    }
+
+    
+    public boolean isCourseText() {
+
+        return courseText;
+    }
+
+    public void printDetails() {
+
+        {
+            System.out.println("Title: " + title);
+            System.out.println("Author: " + author);
+            System.out.println("Number of Pages: " + pages);
+            System.out.println("Amount of times borrowed: " + borrowed);
+            System.out.println("Reference number: " + refNumber);
+            if (refNumber.length() > 0) {
+                System.out.println("Reference number: " + refNumber);
+            } else {
+
+                System.out.println("Reference number: ZZZ");
+            }
+        }
+
+    }
 }
